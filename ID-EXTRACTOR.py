5bbfc8ddc8e0469760caab3e7693483cf2c206cd
hamm-wiss <73249352+HAMM-WISS@users.noreply.github.com>
2020-12-02 23:21:36 +0200

    Update ID-EXTRACTOR.py

--- ID-EXTRACTOR.py
+++ ID-EXTRACTOR.py
@@ -1,5 +1,6 @@
 import amino
 import time
+import os
 green = '\033[0;32m'
 white = '\033[0;37m'
 red = '\033[0;31m'
@@ -11,8 +12,34 @@ time.sleep(2)
 client =amino.Client()
 email=emaillogin
 password = passwordlogin
-client.login(email=email, password=password)
-hostlink=input(green + "ENTER THE LINK : ")
-time.sleep(1)
-id =client.get_from_code(hostlink).objectId
+Gather=0
+if Gather!=1:
+	try:
+		client.login(email=email, password=password)
+	except:
+		print(red+"Invalid email or password!"+white)
+		os._exit(1)
+print("[1]-Chat/User ID")
+print("[2]-Community ID")
+Choice=input(green+"Choose : "+white)
+if choice==1 or choice==01:
+	hostlink=input(green + "ENTER THE LINK : ")
+	time.sleep(1)
+	if len(hostlink)!=0:
+		try:
+			id =client.get_from_code(hostlink).objectId
+		except:
+			print(red+"Invalid url"+white)
+			os._exit(1)
+if choice==2 or choice==02:
+	hostlink=input(green + "ENTER YOUR PROFILE LINK : ")
+	time.sleep(1)
+	if len(hostlink)!=0:
+		try:
+			uurl =client.get_from_code(hostlink).objectId
+			id = uurl.path[1:uurl.path.index("/")]
+		except:
+			print(red+"Invalid url"+white)
+			os._exit(1)
 print(red + 'ID= ' + white + id)
+os._exit(1)
